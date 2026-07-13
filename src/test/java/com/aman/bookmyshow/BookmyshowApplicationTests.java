b3fc30f00c7ddb85f0ca602fb2649501ea91b73f
aman_singh_bhadouriya <abhadouriya501@gmail.com>
2026-07-14 00:43:32 +0530

    starting1

---
+++ src/test/java/com/aman/bookmyshow/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
